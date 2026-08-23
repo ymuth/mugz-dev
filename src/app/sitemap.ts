895db9fc63c4656c78de5642a67f0d53d6067065
ymuth <135130958+ymuth@users.noreply.github.com>
2026-08-23 18:49:14 +0100

    updated sitemap to avoid 308 redirect

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,27 +3,27 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://mugz.dev",
+            url: "https://www.mugz.dev",
             changeFrequency: "weekly",
             priority: 1,
         },
         {
-            url: "https://mugz.dev/services",
+            url: "https://www.mugz.dev/services",
             changeFrequency: "monthly",
             priority: 0.9,
         },
         {
-            url: "https://mugz.dev/about",
+            url: "https://www.mugz.dev/about",
             changeFrequency: "monthly",
             priority: 0.8,
         },
         {
-            url: "https://mugz.dev/contact",
+            url: "https://www.mugz.dev/contact",
             changeFrequency: "monthly",
             priority: 0.8,
         },
         {
-            url: "https://mugz.dev/privacy",
+            url: "https://www.mugz.dev/privacy",
             changeFrequency: "yearly",
             priority: 0.3,
         },
